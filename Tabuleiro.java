54bbda9b24374792fcfb2b7dd9e630b62938e675
Thiago de Oliveira Alves <towo497@gmail.com>
2020-07-03 05:33:07 -0300

    imports organizados

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -4,14 +4,11 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
